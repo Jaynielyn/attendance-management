e6ef0a960897b456c22dbd01641b53e9fb1660c2
jay <myacc.j2@gmail.com>
2025-01-08 15:28:05 +0900

    Fortifyバリデーション

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -8,15 +8,21 @@
 @section('content')
 <div class="login__container">
     <h1 class="login__title">ログイン</h1>
-    <form action="/login" method="POST" class="login__form">
+    <form action="{{ route('login') }}" method="POST" class="login__form" novalidate>
         @csrf
         <div class="form__group">
             <label for="email" class="form__label">メールアドレス</label>
             <input type="email" id="email" name="email" class="form__input" required>
+            @error('email')
+            <div class="error">{{ $message }}</div>
+            @enderror
         </div>
-        <div class="form__group">
+        <div class="form__group form__group-pass">
             <label for="password" class="form__label">パスワード</label>
             <input type="password" id="password" name="password" class="form__input" required>
+            @error('password')
+            <div class="error">{{ $message }}</div>
+            @enderror
         </div>
         <button type="submit" class="form__btn-submit">登録する</button>
     </form>
--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -8,19 +8,28 @@
 @section('content')
 <div class="register__container">
     <h1 class="register__title">会員登録</h1>
-    <form action="/register" method="POST" class="register__form">
+    <form action="{{ route('register') }}" method="POST" class="register__form" novalidate>
         @csrf
         <div class="form__group">
             <label for="name" class="form__label">名前</label>
             <input type="text" id="name" name="name" class="form__input" required>
+            @error('name')
+            <div class="error">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form__group">
             <label for="email" class="form__label">メールアドレス</label>
             <input type="email" id="email" name="email" class="form__input" required>
+            @error('email')
+            <div class="error">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form__group">
             <label for="password" class="form__label">パスワード</label>
             <input type="password" id="password" name="password" class="form__input" required>
+            @error('password')
+            <div class="error">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form__group">
             <label for="password_confirmation" class="form__label">パスワード確認</label>
